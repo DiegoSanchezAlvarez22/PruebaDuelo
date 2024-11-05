6117ee49a64e11b5e2c100371f4c2d40e0d96aad
DiegoSanchezAlvarez22 <146394007+DiegoSanchezAlvarez22@users.noreply.github.com>
2024-11-05 13:57:33 +0100

    fourth commit

--- Assets/Scripts/NuevoScript.cs
+++ Assets/Scripts/NuevoScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraPos : MonoBehaviour
+public class NuevoScript : MonoBehaviour
 {
     [SerializeField] private Transform _target;
 
